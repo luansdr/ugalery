2d1c12fc39d4bae4f05a233877253e4d2ddb85ad
luan.reis <luan.reis@portoseguro.com.br>
2023-10-02 21:42:55 -0300

    Primeiro commit

---
+++ src/test/java/br/com/fiap/ugalery/UgaleryApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fiap.ugalery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UgaleryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
