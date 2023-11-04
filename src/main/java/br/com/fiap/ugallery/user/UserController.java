f8776b1ff5a3d87f0a7d58592ef80113cbff12e9
luan.reis <luan.reis@portoseguro.com.br>
2023-11-04 11:08:09 -0300

    internalização okay

--- src/main/java/br/com/fiap/ugallery/user/UserController.java
+++ src/main/java/br/com/fiap/ugallery/user/UserController.java
@@ -1,13 +1,30 @@
 package br.com.fiap.ugallery.user;
-
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class UserController {
 
+    @Autowired
+    UserRepository repository;
+
+    @GetMapping("/users")
+    public String usersPage(Model model){
+        model.addAttribute("users", repository.findAll());
+        return "auth/users";
+    }
+
+
     @GetMapping("/login")
-    public String loginPage() {
+    public String loginPage(){
         return "auth/login";
     }
+
+    @GetMapping("/logout")
+    public String logoutPage(){
+        return "auth/logout";
+    }
+    
 }
